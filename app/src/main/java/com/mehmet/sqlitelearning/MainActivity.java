f56f72c04be562bde9cb85ce22813c0b995f36c1
Mehmet Emin Yagli <m.eminyagli@gmail.com>
2022-09-15 15:48:39 +0300

    sqlite database'ini eklemeyi öğrendim

--- app/src/main/java/com/mehmet/sqlitelearning/MainActivity.java
+++ app/src/main/java/com/mehmet/sqlitelearning/MainActivity.java
@@ -2,6 +2,7 @@ package com.mehmet.sqlitelearning;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
@@ -10,5 +11,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        try {
+            //SQLiteDatabase database = this.openOrCreateDatabase(datanın ismi , modu(public,private) ,factory)
+            SQLiteDatabase database = this.openOrCreateDatabase("deneme1",MODE_PRIVATE,null);
+            //                       IF NOT EXISTS ==> EĞER BÖYLE BİR TABLO YOKSA OLUŞTUR
+            //                                                     tablo isminden sonra colonları ve veri tiplerini yazmamız gerekiyor
+            database.execSQL("CREATE TABLE IF NOT EXISTS Muzisyenler(name VARCHAR , age INT)");
+
+        } catch (Exception e){
+            e.printStackTrace();
+        }
     }
 }
